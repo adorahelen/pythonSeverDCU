40b8ab5317f7a3d15defc058208c49a703ede1af
adorahelen <adorahelenmin@gmail.com>
2024-05-11 21:05:57 +0900

    3

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
@@ -15,18 +14,14 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    # render_template 함수를 사용하여 base.html을 렌더링하고 클라이언트에게 반환
+    return render_template('base.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('login'))  # 회원가입 성공 시 로그인 페이지로 리다이렉트
-    return render_template('register.html', title='Register', form=form)
+    # render_template 함수를 사용하여 register.html을 렌더링하고 클라이언트에게 반환
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
