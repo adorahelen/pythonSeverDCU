9ab5068f99f1a40afad45f978f207f4404c24489
adorahelen <adorahelenmin@gmail.com>
2024-05-14 14:21:24 +0900

    24-05-14 . MySQL 디비 연동 성공

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+conn = pymysql.connect(host='127.0.0.1', user ='root', password ='0000', db ='soloDB', charset ='utf8')
 
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+cur = conn.cursor()
+#데이터베이스에 SQL 문을 실행하거나 실행된 결과 돌려받는 통로
 
-@app.route('/')
-def home():
-    # render_template 함수를 사용하여 base.html을 렌더링하고 클라이언트에게 반환
-    return render_template('base.html')
+cur.execute("CREATE TABLE userTable(id char(4), userName char(15),email char(20),birthYear int)")
+#테이블을 만드는 SQL 문 : 커서 이름 .execute
 
-@app.route('/register')
-def register():
-    # render_template 함수를 사용하여 register.html을 렌더링하고 클라이언트에게 반환
-    return render_template('register.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
